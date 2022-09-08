3f87fe34dffa0584d2acbe63e0ad8650d977f425
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-08 20:48:31 -0300

    Requisito 29

---
+++ challenges/desafio29.js
@@ -0,0 +1,11 @@
+// 29 - Renomeie o campo descricao para descricaoSite em todos os documentos
+// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+db.produtos.updateMany(
+  {}, { $rename: { descricao: "descricaoSite" } },
+);
+
+// Crie uma query que retorne o nome e descricaoSite de todos os documentos.
+db.produtos.find(
+  {},
+  { nome: 1, descricaoSite: 1, _id: 0 },
+);
