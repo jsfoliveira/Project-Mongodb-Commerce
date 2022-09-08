5d02a3b55bdf23a0cef909feadd6cf10e612bed2
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-08 20:04:04 -0300

    Requisito 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,9 @@
+// 19 - Remova o item cebola de todos os sanduíches
+// Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+db.produtos.updateMany(
+  {},
+  { $pull: { ingredientes: "cebola" } },
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
