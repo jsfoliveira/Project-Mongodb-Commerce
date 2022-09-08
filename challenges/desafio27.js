42fd8ac9c51ba254f930ddbf2911c620b46b8332
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-08 20:45:40 -0300

    Requisito 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,4 +1,5 @@
 // 27 - Conte quantos produtos contém Mc no nome, sem considerar letras maiúsculas ou minúsculas
+// Como é só conter Mc, pode estar em qualquer lugar no nome, fica desse jeito. Mas se quiser encontrar Mc iniciando a palavra, coloca assim: { $regex: /^Mc/i }
 db.produtos.countDocuments({
   nome: { $regex: /Mc/i },
 });
